a2c56999e69b3545ecd61d0b022c63155429c3e6
hafnium49 <hafnium49@gmail.com>
2023-10-26 05:45:58 +0900

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -1,1 +1,21 @@
+# streamlit_app.py
 
+import streamlit as st
+from PIL import Image
+
+def to_gray(img):
+    return img.convert("L")
+
+st.title("Grayscale Image Converter")
+
+uploaded_image = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
+
+if uploaded_image is not None:
+    image = Image.open(uploaded_image)
+    st.image(image, caption="Uploaded Image.", use_column_width=True)
+    st.write("")
+    st.write("Processing...")
+
+    gray_image = to_gray(image)
+    
+    st.image(gray_image, caption="Grayscaled Image.", use_column_width=True)
